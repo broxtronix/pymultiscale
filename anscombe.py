08094445798f0bafedb6a436acd5a34eae2c60cd
Michael Broxton <broxton@gmail.com>
2014-07-30 20:56:35 -0700

    Tweaks and cleanup to SIRT, RL, and AMP.

--- anscombe.py
+++ anscombe.py
@@ -1,10 +1,26 @@
 import numpy as np
+#
+# The function in this file follow closely after the MATLAB scripts
+# written by Alessandro Foi and Markku Makitalo - Tampere University
+# of Technology - 2011-2012
+#
+#    http://www.cs.tut.fi/~foi/invansc/
+#
+# that accompanies their paper:
+#
+#    Reference: M. Makitalo and A. Foi, "Optimal inversion of the
+#    generalized Anscombe transformation for Poisson-Gaussian noise",
+#    IEEE Trans. Image Process., doi:10.1109/TIP.2012.2202675
+
 
 #-------- Variance stabilizing transforms -----------
 
 def anscombe(x):
     '''
     Compute the anscombe variance stabilizing transform.
+
+      the input   x   is noisy Poisson-distributed data
+      the output  fx  has variance approximately equal to 1.
 
     Reference: Anscombe, F. J. (1948), "The transformation of Poisson,
     binomial and negative-binomial data", Biometrika 35 (3-4): 246-254
@@ -32,31 +48,52 @@ def generalized_anscombe(x, mu, sigma, gain=1.0):
     which assumes that the data provided to it is a mixture of poisson
     and gaussian noise.
 
+    The input signal  z  is assumed to follow the Poisson-Gaussian noise model
+
+        x = gain * p + n
+
+    where gain is the camera gain and mu and sigma are the read noise
+    mean and standard deviation.
+
     We assume that x contains only positive values.  Values that are
     less than or equal to 0 are ignored by the transform.
 
     Note, this transform will show some bias for counts less than
     about 20.
     '''
-    result = x.copy()
-
-    positive_idxs = np.where(x > 0.0)
-    y = gain*x[positive_idxs] + (gain**2)*3.0/8.0 + sigma**2 - gain*mu
+    y = gain*x + (gain**2)*3.0/8.0 + sigma**2 - gain*mu
 
     # Clamp to zero before taking the square root.
-    y[y < 0.0] = 0.0
-    y_stabilized = (2.0/gain)*np.sqrt(y)
-
-    # Replace values in original array and return result
-    result[positive_idxs] = y_stabilized
-    return result
+    return (2.0/gain)*np.sqrt(np.maximum(y, 0.0))
 
 def inverse_generalized_anscombe(x, mu, sigma, gain=1.0):
-    result = x.copy()
+    '''
+    Applies the closed-form approximation of the exact unbiased
+    inverse of Generalized Anscombe variance-stabilizing
+    transformation.
 
-    positive_idxs = np.where(x > 0.0)
-    y = (1.0/gain)*(gain*x[positive_idxs]/2.0)**2 - gain*3.0/8.0 - (sigma**2)/gain + mu
+    The input signal x is transform back into a Poisson random variable
+    based on the assumption that the original signal from which it was
+    derived follows the Poisson-Gaussian noise model:
 
-    # Replace values in original array and return result
-    result[positive_idxs] = y
-    return result
+        x = gain * p + n
+
+    where gain is the camera gain and mu and sigma are the read noise
+    mean and standard deviation.
+
+    Roference: M. Makitalo and A. Foi, "Optimal inversion of the
+    generalized Anscombe transformation for Poisson-Gaussian noise",
+    IEEE Trans. Image Process., doi:10.1109/TIP.2012.2202675
+
+    '''
+    test = np.maximum(x, 1.0)
+    exact_inverse = ( np.power(test/2.0, 2.0) +
+                      1.0/4.0 * np.sqrt(3.0/2.0)*np.power(test, -1.0) -
+                      11.0/8.0 * np.power(test, -2.0) +
+                      5.0/8.0 * np.sqrt(3.0/2.0) * np.power(test, -3.0) -
+                      1.0/8.0 - np.power(sigma, 2) )
+    exact_inverse = np.maximum(0.0, exact_inverse)
+    exact_inverse *= gain
+    exact_inverse += mu
+    exact_inverse[np.where(exact_inverse != exact_inverse)] = 0.0
+    return exact_inverse
